4428845480392d210bec003b6fcb68757b8aa849
moctar1403 <34677332+moctar1403@users.noreply.github.com>
2023-02-09 17:02:49 +0000

    Middilware login add

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 class LoginController extends Controller
 {
@@ -26,7 +29,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
+    protected function authenticated() {
+         if (Auth::user()->role_as=='1') {
+           return redirect('/admin/dashboard')->with('message','Welcome to Dashboard');
+        }
+        else{
+            return redirect('/home')->with('status','Logged in succeful');
+        }
+
+    }
 
     /**
      * Create a new controller instance.
